2d2ee8c65ce945a74a6f6e66e783be6014709988
Arshiya <arshiya1383.mk@gmail.com>
2022-07-11 14:37:09 +0430

    finish responsive layout

--- script.js
+++ script.js
@@ -2,12 +2,16 @@
 document.addEventListener("click", (e) => {
   const isDropDownButton = e.target.matches("[data-dropDown-button]");
   const dropDown = e.target.closest("[data-dropDown]");
+  const isHamburger=e.target.matches("[data-hamburger]")
   if (!isDropDownButton && dropDown) return;
 
   let currentDropDown;
   if (isDropDownButton) {
     dropDown.classList.toggle("active");
     currentDropDown = dropDown;
+
+   
+    
   }
 
   document.querySelectorAll("[data-dropDown].active").forEach((dropDown) => {
@@ -15,4 +19,10 @@ document.addEventListener("click", (e) => {
 
     dropDown.classList.remove("active");
   });
+  if(isHamburger){
+    const hamburgerSibling=e.target.nextElementSibling
+    e.target.classList.toggle("animate")
+    hamburgerSibling.classList.toggle("slide")
+
+  }
 });
